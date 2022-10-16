18860c93df8baf641b83f635bf5706c98821f955
Mohammad Hasibul Hasan <109550801+hasibulhimu49@users.noreply.github.com>
2022-10-17 01:16:29 +0600

    Add files via upload

---
+++ Problem solved/71A-Way too long words.cpp
@@ -0,0 +1,22 @@
+//Codeforces problem no-71A
+//Problem name-way too long words
+
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    string s;
+    int n;
+    cin>>n;
+    for(int i=0; i<n; i++)
+    {
+        cin>>s;
+        if(s.length()>10)
+        {
+            cout<<s[0]<<s.length()-2<<s[s.length()-1]<<endl;
+        }
+        else
+            cout<<s<<endl;
+    }
+}
