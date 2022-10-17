f660e37a10ceac0a1755c067919c13b6062e4630
Mohammad Hasibul Hasan <109550801+hasibulhimu49@users.noreply.github.com>
2022-10-18 02:18:54 +0600

    Add files via upload

---
+++ Problem solved/112A-Petya and Strings.cpp
@@ -0,0 +1,23 @@
+//Codeforces problem no-112A
+//Problem name-Petya and Strings
+
+#include <iostream>
+#include<string.h>
+using namespace std;
+string a, b;
+int main() {
+    cin >> a >> b;
+    for (int i = 0; i < a.size(); i++) {
+        a[i] = tolower(a[i]);
+        b[i] = tolower(b[i]);
+        }
+
+    if (a < b) {
+        cout << -1;
+    } else if (a > b) {
+        cout << 1;
+    } else if (a == b) {
+        cout << 0;
+    }
+    return 0;
+}
