ee090e0c1bbc4d132e09c6a7574534194ee07a60
Mohammad Hasibul Hasan <109550801+hasibulhimu49@users.noreply.github.com>
2022-10-18 00:29:43 +0600

    Add files via upload

---
+++ Problem solved/231A-Team.cpp
@@ -0,0 +1,22 @@
+//Codeforces problem no-231A
+//Problem name-Team
+
+#include<iostream>
+using namespace std;
+int main(){
+int n, P, V, T, c=0;
+
+cin>>n;
+
+while(n--) {
+
+    cin >> P >> V >> T;
+
+    if(P + V + T >= 2)
+    {
+        c+=1;
+    }
+}
+cout << c << endl;
+}
+
