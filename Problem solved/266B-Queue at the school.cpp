074217d2c20b6ee33cb2cb121818a579818062a6
Mohammad Hasibul Hasan <109550801+hasibulhimu49@users.noreply.github.com>
2022-10-18 03:38:20 +0600

    Add files via upload

---
+++ Problem solved/266B-Queue at the school.cpp
@@ -0,0 +1,27 @@
+//Codeforces problem no-226B
+//Problem name-Queue at the school
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+int n,t;
+string s;
+cin>>n>>t>>s;
+while(t--)
+{
+for(int i=1;i<n;i++)
+{
+if(s[i]=='G' && s[i-1]=='B')
+{
+s[i]='B';
+s[i-1]='G';
+i++;
+}
+}
+}
+cout<<s<<endl;
+return 0;
+}
