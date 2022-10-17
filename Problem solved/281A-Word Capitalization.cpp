5f2d81ccee91d6e3407a9ece404a85ed39b79774
Mohammad Hasibul Hasan <109550801+hasibulhimu49@users.noreply.github.com>
2022-10-18 02:33:52 +0600

    Add files via upload

---
+++ Problem solved/281A-Word Capitalization.cpp
@@ -0,0 +1,15 @@
+//Codeforces problem no-281A
+//Problem name-Word Capitalization
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    char word[1000];
+    cin>>word;
+    word[0]=toupper(word[0]);
+    cout<<word<<endl;
+
+    return 0;
+}
