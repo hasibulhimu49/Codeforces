f61f20ac6a3a99ab6dc0602ad16152b1cb9fa930
Mohammad Hasibul Hasan <109550801+hasibulhimu49@users.noreply.github.com>
2022-10-18 00:30:03 +0600

    Add files via upload

---
+++ Problem solved/158A-Next Round.cpp
@@ -0,0 +1,19 @@
+//Codeforces problem no-158A
+//Problem name-Next Round
+
+#include<iostream>
+using namespace std;
+int x, k, a, c=0, n[1000];
+int main(){
+cin >> x >> k;
+for(int i=1; i<=x; i++){
+    cin>>a;
+    n[i] = a;
+}
+for(int j=1; j<=x; j++)
+{
+    if(n[j]>=n[k] && n[j]>0)
+        c++;
+}
+cout << c << endl;
+}
