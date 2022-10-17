2b6fb385c04901dd34d703f99a57677120d0e20e
Mohammad Hasibul Hasan <109550801+hasibulhimu49@users.noreply.github.com>
2022-10-18 00:30:30 +0600

    Add files via upload

---
+++ Problem solved/50A-Domino piling.cpp
@@ -0,0 +1,16 @@
+//Codeforces problem no-50A
+//Problem name-Domino piling
+
+#include<iostream>
+using namespace std;
+int main(){
+
+    int m,n,s;
+    cin>> m>> n;
+    if(m>=1 && m<=n && n<=16)
+    {
+        s=(m*n)/2;
+    }
+    cout<<s<<endl;
+
+}
