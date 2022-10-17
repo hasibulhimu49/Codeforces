2b3c4f0f6f1584f56204aef50da5822d99cb803c
Mohammad Hasibul Hasan <109550801+hasibulhimu49@users.noreply.github.com>
2022-10-18 03:37:54 +0600

    Add files via upload

---
+++ Problem solved/116A-Tram.cpp
@@ -0,0 +1,26 @@
+//Codeforces problem no-116A
+//Problem name-Tram
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+int n;
+cin >> n;
+int a,b;
+int sum=0,k=0;
+for(int i=0;i< n;i++)
+{
+cin>>a>>b;
+sum=(a + k) - b;
+if(sum < 0)
+{
+k=0;
+}
+else
+k = sum;
+}
+cout<<sum<<endl;
+return 0;
+}
