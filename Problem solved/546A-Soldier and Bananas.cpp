183ccb372112d082322af9fd90153e4995062186
Mohammad Hasibul Hasan <109550801+hasibulhimu49@users.noreply.github.com>
2022-10-18 03:13:26 +0600

    Add files via upload

---
+++ Problem solved/546A-Soldier and Bananas.cpp
@@ -0,0 +1,22 @@
+//Codeforces problem no-546A
+//Problem name-Soldier and Bananas
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int k, n, w;
+    cin >> k >> n >> w;
+    int sum = 0;
+    for(int i = 1; i <= w; i++)
+    {
+        sum += i;
+    }
+    int d = (sum * k) - n;
+    if(d < 0)
+        cout << "0";
+    else
+        cout << d;
+
+}
+
