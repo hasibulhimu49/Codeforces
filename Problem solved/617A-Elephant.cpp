0f0d7f04ea10cb0c7e09abf2a59d486c1e71c772
Mohammad Hasibul Hasan <109550801+hasibulhimu49@users.noreply.github.com>
2022-10-18 03:25:10 +0600

    Add files via upload

---
+++ Problem solved/617A-Elephant.cpp
@@ -0,0 +1,21 @@
+//Codeforces problem no-617A
+//Problem name-Elephant
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x;
+    cin>>x;
+
+    if(x%5 == 0){
+        cout<<x/5<<endl;
+    }
+    else
+    {
+        cout<<x/5+1<<endl;
+    }
+}
+
+
